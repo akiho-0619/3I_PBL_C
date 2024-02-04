e5ca42c99b6265749c98185d98d449e141619190
akiho-0619 <aki19.akiho@gmail.com>
2024-02-04 09:42:41 +0900

    com

--- module/text_umekomi.js
+++ module/text_umekomi.js
@@ -26,19 +26,20 @@ fs.readFile('module/pdf/output.txt', 'utf-8', function (err, data) {
         return;
     }
 
-    const test_p= document.getElementById("p#yotei_space")
-    test_p.textContent("test_text")
-    var get_text = data.split("\r\n");
-    //console.log(get_text);
-    var yotei_ran = new Array(get_text.length);
-    for (var i = 0; i < get_text.length; i++) {
-        yotei_ran[i] = document.createElement("label");
-        yotei_ran[i].textContent = get_text[i];
-    }
-    console.log("output");
-    for (var _i = 0, yotei_ran_1 = yotei_ran; _i < yotei_ran_1.length; _i++) {
-        var i = yotei_ran_1[_i];
-        document.querySelector("p#yotei_space").appendChild(i);
-    }
+    
 });
 
+const test_p= document.getElementById("p#yotei_space")
+test_p.textContent("test_text")
+var get_text = data.split("\r\n");
+//console.log(get_text);
+var yotei_ran = new Array(get_text.length);
+for (var i = 0; i < get_text.length; i++) {
+    yotei_ran[i] = document.createElement("label");
+    yotei_ran[i].textContent = get_text[i];
+}
+console.log("output");
+for (var _i = 0, yotei_ran_1 = yotei_ran; _i < yotei_ran_1.length; _i++) {
+    var i = yotei_ran_1[_i];
+    document.querySelector("p#yotei_space").appendChild(i);
+}
