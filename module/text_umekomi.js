77001436928a4b581cabcb8c2b00bd88b5ba2c69
akiho-0619 <aki19.akiho@gmail.com>
2024-02-04 16:49:25 +0900

    addf

--- module/text_umekomi.js
+++ module/text_umekomi.js
@@ -39,7 +39,7 @@ function text_sousa(fileContent){
     var match_text=Array();
     for(var i=0; i<get_text.length; i++){
         var now_text= get_text[i];
-        var text_day= parseInt("3" ? parseInt(get_text.split("日")[0].split("月")[0])<=3: "2" + get_text.split("日")[0].split("月")[0].padStart(2, "0")+  get_text.split("日")[0].split("月")[1].padStart(2, "0"));
+        var text_day= parseInt("3" ? parseInt(now_text.split("日")[0].split("月")[0])<=3: "2" + now_text.split("日")[0].split("月")[0].padStart(2, "0")+  now_text.split("日")[0].split("月")[1].padStart(2, "0"));
         if( formattedDate <= text_day <= (formattedDate+100 ? formattedDate+100 > 21231: formattedDate-1100+10000)){
             match_text.push(now_text);
         }
