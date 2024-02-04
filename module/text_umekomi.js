5125673723354360fe41c5a64b87896b54c87994
akiho-0619 <aki19.akiho@gmail.com>
2024-02-04 16:39:22 +0900

    add

--- module/text_umekomi.js
+++ module/text_umekomi.js
@@ -37,7 +37,7 @@ function text_sousa(fileContent){
     //console.log(formattedDate);
     var get_text = fileContent.split("\r\n");
     var match_text=Array();
-    for(i=0; i<get_text.length; i++){
+    for(var i=0; i<get_text.length; i++){
         var now_text= get_text[i];
         var text_day= parseInt("3" ? parseInt(get_text.split("日")[0].split("月")[0])<=3: "2" + get_text.split("日")[0].split("月")[0].padStart(2, "0")+  get_text.split("日")[0].split("月")[1].padStart(2, "0"));
         if( formattedDate <= text_day <= (formattedDate+100 ? formattedDate+100 > 21231: formattedDate-1100+10000)){
